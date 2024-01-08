59d37ca5df47520e84cb8074b7efc6d2e9668b6a
Janani495 <d.janani95@gmail.com>
2024-01-08 17:43:16 +0530

    First Time Push

---
+++ src/test/java/com/hcl/OrderA.java
@@ -0,0 +1,19 @@
+package com.hcl;
+
+public class OrderA {
+
+	public void empName1() {
+		System.out.println("Rajesh");
+		
+	}
+	public void empName2() {
+		System.out.println("Laksh");
+		
+	}
+	public void empName3() {
+		System.out.println("Rahul");
+		
+	}
+	
+	
+}
---
+++ src/test/java/com/hcl/OrderB.java
@@ -0,0 +1,17 @@
+package com.hcl;
+
+public class OrderB {
+	public void empId1() {
+		System.out.println("R0011");
+		
+	}
+	public void empId2() {
+		System.out.println("L0012");
+		
+	}
+	public void empId3() {
+		System.out.println("R0013");
+		
+	}
+	
+}
